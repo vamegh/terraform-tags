7f0a2cd6aa01be89d61af86e609d78d7240364c2
vam <87972568+vam-noths@users.noreply.github.com>
2021-10-13 15:28:56 +0100

    Complete overhaul of tagging module.
    Now using experimental optional attribute, it now takes in just 3
    variables (tags, extra_tags and additional_tag_maps)
    
    The tags can now be passed into a map and the keys it understand it will
    convert properly into pascal case and present out along with it being
    presented as a list of maps for resources which require that format.
    
    also re-organised to match other module layouts.
    
    	new file:   data.tf
    	new file:   locals.tf
    	modified:   main.tf
    	modified:   outputs.tf
    	modified:   variables.tf

---
+++ locals.tf
@@ -0,0 +1,150 @@
+locals {
+  tags = defaults(var.tags, {
+    account_name     = ""
+    application      = ""
+    app_version      = ""
+    business_owner   = ""
+    classification   = ""
+    common_tags      = ""
+    cost_center      = ""
+    data             = ""
+    docs             = ""
+    env              = ""
+    name             = ""
+    origin           = ""
+    os               = ""
+    os_version       = ""
+    project          = ""
+    service          = ""
+    source_ami       = ""
+    sub_env          = ""
+    type             = ""
+    update_frequency = ""
+    github_org       = ""
+    github_repo      = ""
+  })
+}
+
+locals {
+  account_name = local.tags.account_name == "" ? {} : {
+    AccountName = local.tags.account_name
+  }
+
+  application = local.tags.application == "" ? {} : {
+    Application = local.tags.application
+  }
+
+  app_version = local.tags.application_version == "" ? {} : {
+    ApplicationVersion = local.tags.application_version
+  }
+
+  business_owner = local.tags.business_owner == "" ? {} : {
+    BusinessOwner = local.tags.business_owner
+  }
+
+  classification = local.tags.classification == "" ? {} : {
+    ClassificationLevel = local.tags.classification
+  }
+
+  common_tags = {
+    ManagedBy  = "terraform",
+    CreatorARN = data.aws_caller_identity.current.arn,
+    CreatorID  = data.aws_caller_identity.current.account_id
+  }
+
+  cost_center = local.tags.cost_center == "" ? {} : {
+    CostCenter = local.tags.cost_center
+  }
+
+  data = local.tags.data == "" ? {} : {
+    SourceData = local.tags.data
+  }
+
+  docs = local.tags.documentation == "" ? {} : {
+    Documentation = local.tags.documentation
+  }
+
+  env = local.tags.environment == "" ? {} : {
+    SourceEnvironment = local.tags.environment
+  }
+
+  name = local.tags.name == "" ? {} : {
+    Name = local.tags.name
+  }
+
+  origin = local.tags.origin == "" ? {} : {
+    Origin = local.tags.origin
+  }
+
+  os = local.tags.os == "" ? {} : {
+    OS = local.tags.os
+  }
+
+  os_version = local.tags.os_version == "" ? {} : {
+    OSVersion = local.tags.os_version
+  }
+
+  project = local.tags.project == "" ? {} : {
+    Project = local.tags.project
+  }
+
+  service = local.tags.service == "" ? {} : {
+    Service = local.tags.service
+  }
+
+  source_ami = local.tags.source_ami == "" ? {} : {
+    SourceAMI = local.tags.source_ami
+  }
+
+  sub_env = local.tags.sub_env == "" ? {} : {
+    SubEnvironment = local.tags.sub_env
+  }
+
+  type = local.tags.type == "" ? {} : {
+    Type = local.tags.type
+  }
+
+  update_frequency = local.tags.update_frequency == "" ? {} : {
+    UpdateFrequency = local.tags.update_frequency
+  }
+
+  github_repo = local.tags.github_repo == "" ? {} : {
+    GitHubRepo = local.tags.github_repo
+  }
+
+  github_org = local.tags.github_org == "" ? {} : {
+    GitHubOrg = local.tags.github_org
+  }
+}
+
+locals {
+  tags = merge(
+    var.extra_tags,
+    local.account_name,
+    local.application,
+    local.app_version,
+    local.business_owner,
+    local.classification,
+    local.common_tags,
+    local.cost_center,
+    local.data,
+    local.docs,
+    local.env,
+    local.name,
+    local.origin,
+    local.os,
+    local.os_version,
+    local.project,
+    local.service,
+    local.source_ami,
+    local.sub_env,
+    local.type,
+    local.update_frequency,
+    local.github_org,
+    local.github_repo
+  )
+}
+
+locals {
+  tags_as_list_of_maps = [for key, value in local.tags : merge({ key = key, value = value }, var.additional_tag_maps)]
+}
--- variables.tf
+++ variables.tf
@@ -1,46 +1,7 @@
-variable "account_name" {
-  type    = string
-  default = ""
-}
-
-variable "application" {
-  type    = string
-  default = ""
-}
-
-variable "application_version" {
-  type    = string
-  default = ""
-}
-
-variable "business_owner" {
-  type    = string
-  default = ""
-}
-
-variable "classification" {
-  type    = string
-  default = ""
-}
-
-variable "cost_center" {
-  type    = string
-  default = ""
-}
-
-variable "data" {
-  type    = string
-  default = ""
-}
-
-variable "documentation" {
-  type    = string
-  default = ""
-}
-
-variable "environment" {
-  type    = string
-  default = ""
+variable "additional_tag_maps" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags to add to list of maps output expected format: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group#example-usage"
 }
 
 variable "extra_tags" {
@@ -49,69 +10,54 @@ variable "extra_tags" {
   description = "Extra / custom tags to include with tagging output"
 }
 
-variable "github_org" {
-  type    = string
-  default = ""
+variable "tags" {
+  description = "A map of tags, the keys get formatted into PascalCase"
+  type = object({
+    account_name     = optional(string)
+    application      = optional(string)
+    app_version      = optional(string)
+    business_owner   = optional(string)
+    classification   = optional(string)
+    common_tags      = optional(string)
+    cost_center      = optional(string)
+    data             = optional(string)
+    docs             = optional(string)
+    env              = optional(string)
+    name             = optional(string)
+    origin           = optional(string)
+    os               = optional(string)
+    os_version       = optional(string)
+    project          = optional(string)
+    service          = optional(string)
+    source_ami       = optional(string)
+    sub_env          = optional(string)
+    type             = optional(string)
+    update_frequency = optional(string)
+    github_org       = optional(string)
+    github_repo      = optional(string)
+  })
+  default = {
+    account_name     = ""
+    application      = ""
+    app_version      = ""
+    business_owner   = ""
+    classification   = ""
+    common_tags      = ""
+    cost_center      = ""
+    data             = ""
+    docs             = ""
+    env              = ""
+    name             = ""
+    origin           = ""
+    os               = ""
+    os_version       = ""
+    project          = ""
+    service          = ""
+    source_ami       = ""
+    sub_env          = ""
+    type             = ""
+    update_frequency = ""
+    github_org       = ""
+    github_repo      = ""
+  }
 }
-
-variable "github_repo" {
-  type    = string
-  default = ""
-}
-
-variable "name" {
-  type    = string
-  default = ""
-}
-
-variable "origin" {
-  type    = string
-  default = ""
-}
-
-variable "os" {
-  description = "Acceptable values: Linux | Windows"
-  default     = ""
-}
-
-variable "os_version" {
-  description = "Version of the OS. Examples: 2012R2, 17.04, 2017.5.19"
-  default     = ""
-}
-
-variable "project" {
-  description = "Project Name This Service / Resource Belongs to"
-  default     = ""
-}
-
-variable "service" {
-  type    = string
-  default = ""
-}
-
-variable "source_ami" {
-  description = "AMI ID of the source AMI that this AMI was built from"
-  default     = ""
-}
-
-variable "sub_env" {
-  description = "Sub-Environment"
-  default     = ""
-}
-
-variable "type" {
-  type    = string
-  default = ""
-}
-
-variable "update_frequency" {
-  type    = string
-  default = ""
-}
-
-variable "additional_tag_maps" {
-  type        = map(string)
-  default     = {}
-  description = "Additional tags to add to list of maps output expected format: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group#example-usage" 
-}
-
